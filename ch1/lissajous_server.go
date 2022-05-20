f212e4a550cdfb49d77fce0dbc444d24bf6fccc6
Antonio Canseco Negri <antonio@canseco.tech>
2022-05-20 14:51:53 +0200

    exercise_01_12

--- ch1/lissajous_server.go
+++ ch1/lissajous_server.go
@@ -4,11 +4,17 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		lissajous(w)
+		cycles, ok := r.URL.Query()["cycles"]
+		if !ok || len(cycles[0]) < 1 {
+			log.Println("URL Param 'cycles' is missing")
+		}
+		c, _ := strconv.Atoi(cycles[0])
+		lissajous(w, c)
 	})
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
